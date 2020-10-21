cffb311eaffd0903cf2c83e3cb9678ef6a987f0c
mikashii <carolina.portas@outlook.com>
2020-10-20 22:54:46 -0300

    Add contact

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const url = 'https://api.github.com/users/mikashii';
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", url , true);
+xhttp.send()
+
+xhttp.onreadystatechange = function () {
+    if(xhttp.readyState == 4 && xhttp.status == 200){
+        console.log(xhttp.responseText);
+    }
+}
+xhttp.send()
+
+
 document.querySelector('#scrollTop').addEventListener('click', (event) => {
     alert('button click')
 })
